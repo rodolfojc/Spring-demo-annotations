efb14407dd35f86292e673043fe54ac564fa10bc
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-01 09:27:44 +0000

    Inject Values - Properties File
    Pre - Demo

--- src/com/springdemo/SportConfig.java
+++ src/com/springdemo/SportConfig.java
@@ -3,9 +3,11 @@ package com.springdemo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 //@ComponentScan("com.springdemo")
+@PropertySource("classpath:sport.properties")
 public class SportConfig {
 
 	//DEFINE BEAN FOR SAD FORTUNE
